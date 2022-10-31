26986e8049397909cbc91c2f34f9e0bcef8bd5b4
mohsenamoon <dramatic225@gmail.com>
2022-10-31 13:30:26 +0330

    Added group gems

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -1,3 +1,5 @@
+import datetime
+
 from base import BaseAgent, Action
 from phase1 import Phase1
 
@@ -5,8 +7,13 @@ from phase1 import Phase1
 class Agent(BaseAgent):
 
     def do_turn(self) -> Action:
+        now1 = datetime.datetime.now()
         phase1 = Phase1(self)
-        return phase1.main()
+        print(self.prev_gem)
+        action = phase1.main()
+        now2 = datetime.datetime.now()
+        print(f'total_seconds: {(now2 - now1).total_seconds()}')
+        return action
 
 
 if __name__ == '__main__':
--- src/python_client/phase1.py
+++ src/python_client/phase1.py
@@ -1,9 +1,7 @@
-from operator import ne
+import numpy as np
+
 from base import Action
 from utils.config import GEMS
-
-import random
-import numpy as np
 
 
 class Phase1:
@@ -22,8 +20,10 @@ class Phase1:
             self.agent.door_indexes = self.make_door_indexes()
         if 'barbed_indexes' not in self.agent.__dict__:
             self.agent.barbed_indexes = self.make_barbed_indexes()
-        if 'gem_distances' not in self.agent.__dict__:
-            self.agent.gem_distances = self.make_gem_distances()
+        if 'gem_distances' not in self.agent.__dict__ and 'sorted_gem_distances' not in self.agent.__dict__:
+            self.agent.sorted_gem_distances, self.agent.gem_distances = self.make_gem_distances()
+        if 'gem_groups' not in self.agent.__dict__:
+            self.agent.gem_groups = self.group_gems()
         if 'prev_gem' not in self.agent.__dict__:
             self.agent.prev_gem = None
         self.scores_env = self.create_score_env()
@@ -100,28 +100,20 @@ class Phase1:
 
         return gem_indexes
 
+    # Will be called only once.
     def make_wall_indexes(self) -> np.array:
-        agent_index = np.empty((0, 2), dtype=int)
         wall_indexes = np.empty((0, 2), dtype=int)  # row, col
-
         for row in range(self.map.shape[0]):
-            agent = np.where(self.map[row] == 'EA')
-            if len(agent[0]) != 0:
-                agent_index = np.vstack((agent_index, [row, agent[0][0]]))
-                self.agent.agent_index = agent_index
             new_arr = np.where(self.map[row] == 'W')
             for col in new_arr[0]:
                 wall_indexes = np.vstack((wall_indexes, [row, col]))
         return wall_indexes
+
+    # Will be called only once.
     def make_key_indexes(self) -> np.array:
-        agent_index = np.empty((0, 2), dtype=int)
-        key_indexes = np.empty((0, 3), dtype=int)  # row, col, key_color
+        key_indexes = np.empty((0, 3), dtype=str)  # row, col, key_color
 
         for row in range(self.map.shape[0]):
-            agent = np.where(self.map[row] == 'EA')
-            if len(agent[0]) != 0:
-                agent_index = np.vstack((agent_index, [row, agent[0][0]]))
-                self.agent.agent_index = agent_index
             new_arr = np.where(self.map[row] == 'r')
             for col in new_arr[0]:
                 key_indexes = np.vstack((key_indexes, [row, col, 'r']))
@@ -134,15 +126,11 @@ class Phase1:
 
         return key_indexes
 
+    # Will be called only once.
     def make_door_indexes(self) -> np.array:
-        agent_index = np.empty((0, 2), dtype=int)
-        door_indexes = np.empty((0, 3), dtype=int)  # row, col, door_color
+        door_indexes = np.empty((0, 3), dtype=str)  # row, col, door_color
 
         for row in range(self.map.shape[0]):
-            agent = np.where(self.map[row] == 'EA')
-            if len(agent[0]) != 0:
-                agent_index = np.vstack((agent_index, [row, agent[0][0]]))
-                self.agent.agent_index = agent_index
             new_arr = np.where(self.map[row] == 'R')
             for col in new_arr[0]:
                 door_indexes = np.vstack((door_indexes, [row, col, 'R']))
@@ -155,21 +143,17 @@ class Phase1:
 
         return door_indexes
 
+    # Will be called only once.
     def make_barbed_indexes(self) -> np.array:
-        agent_index = np.empty((0, 2), dtype=int)
         barbed_indexes = np.empty((0, 2), dtype=int)  # row, col
-
         for row in range(self.map.shape[0]):
-            agent = np.where(self.map[row] == 'EA')
-            if len(agent[0]) != 0:
-                agent_index = np.vstack((agent_index, [row, agent[0][0]]))
-                self.agent.agent_index = agent_index
             new_arr = np.where(self.map[row] == '*')
             for col in new_arr[0]:
                 barbed_indexes = np.vstack((barbed_indexes, [row, col]))
         return barbed_indexes
+
     # Will be called only once.
-    def make_gem_distances(self) -> np.array:
+    def make_gem_distances(self) -> list:
         distances = np.array([])
         agent_row = self.agent.agent_index[0][0]
         agent_col = self.agent.agent_index[0][1]
@@ -181,42 +165,105 @@ class Phase1:
             distances = np.append(distances, distance)
 
         sorted_distances = np.sort(distances)
-        return sorted_distances
-    def calc_neighbor(self , i_agent , j_agent) -> np.array :
-        print("yess")
-        neighbor = np.zeros((3,3), dtype=int)
+        return [sorted_distances, distances]
 
-        for i in range(i_agent-1 , i_agent+2):
-            for j in range(j_agent-1,j_agent+2):
-                if self.map[i][j] == 'E' :
-                    if(i == i_agent) or (j == j_agent):
+    def calc_neighbor(self, i_agent, j_agent) -> np.array:
+        print("yess")
+        neighbor = np.zeros((3, 3), dtype=int)
+
+        for i in range(i_agent - 1, i_agent + 2):
+            for j in range(j_agent - 1, j_agent + 2):
+                if self.map[i][j] == 'E':
+                    if (i == i_agent) or (j == j_agent):
                         neighbor[i][j] += -1
-                    else :
+                    else:
                         neighbor[i][j] += -2
-                if self.map[i][j] == 'W' :
+                if self.map[i][j] == 'W':
                     neighbor[i][j] += -10000
-                if self.map[i][j] == 'g' or self.map[i][j] == 'r' or self.map[i][j] == 'y' :
+                if self.map[i][j] == 'g' or self.map[i][j] == 'r' or self.map[i][j] == 'y':
                     neighbor[i][j] += 10
-                if self.map[i][j] == '*' :
+                if self.map[i][j] == '*':
                     neighbor[i][j] += -20
-                if self.map[i][j] == 'G' or self.map[i][j] == 'R' or self.map[i][j] == 'Y' :
+                if self.map[i][j] == 'G' or self.map[i][j] == 'R' or self.map[i][j] == 'Y':
                     # if we have key : 
                     neighbor[i][j] += 10
                     # we dont have key :
-                    neighbor[i][j] += -10000    
-
-
-                                                 
-
-
+                    neighbor[i][j] += -10000
 
         return neighbor
-        
 
+    def search_nearby_cells(self, cell: np.array, new_gem_indexes: np.array, gem_group: np.array) -> np.array:
+        grid_height = self.height
+        grid_width = self.width
+        row = cell[0]
+        col = cell[1]
+        for new_index in new_gem_indexes:
+            nearby_cells = np.empty((0, 2))
+            if col != grid_height:
+                nearby_cells = np.vstack((nearby_cells, [row, col + 1]))
+            if col != 0:
+                nearby_cells = np.vstack((nearby_cells, [row, col - 1]))
+            if row != 0 and col != 0:
+                nearby_cells = np.vstack((nearby_cells, [row - 1, col - 1]))
+            if row != 0:
+                nearby_cells = np.vstack((nearby_cells, [row - 1, col]))
+            if row != 0 and col != grid_height:
+                nearby_cells = np.vstack((nearby_cells, [row - 1, col + 1]))
+            if row != grid_width and col != 0:
+                nearby_cells = np.vstack((nearby_cells, [row + 1, col - 1]))
+            if row != grid_width:
+                nearby_cells = np.vstack((nearby_cells, [row + 1, col]))
+            if row != grid_width and col != grid_height:
+                nearby_cells = np.vstack((nearby_cells, [row + 1, col + 1]))
+
+            reshaped_nearby_cells = [x.tolist() for x in nearby_cells]
+            for num, cl in enumerate(reshaped_nearby_cells):
+                cl.append(new_index[2])
+                reshaped_nearby_cells[num] = [int(x) for x in cl]
+            if new_index.tolist() in reshaped_nearby_cells:
+                gem_group = np.vstack((gem_group, new_index.astype(int)))
+
+        return gem_group
+
+    def search_gems(self, index: np.array, gem_group: np.array, searched_gems: np.array, gem_groups: list,
+                    gem_indexes: np.array) -> list:
+        new_gem_indexes = np.array(
+            [x for x in gem_indexes.tolist() if x not in searched_gems.tolist() and x not in gem_group.tolist()])
+        # if len(gem_group) == len(searched_gems):
+        #     gem_groups.append(gem_group)  # break the loop
+        #     return gem_groups
+        gem_group = self.search_nearby_cells(index, new_gem_indexes, gem_group)
+        searched_gems = np.vstack((searched_gems, index))
+
+        not_searched_gems = [x for x in gem_group.tolist() if x not in searched_gems.tolist()]
+        if len(not_searched_gems) == 0:
+            gem_groups.append(gem_group)
+            return gem_groups
+        index = not_searched_gems[0]
+        gem_groups = self.search_gems(index, gem_group, searched_gems, gem_groups, gem_indexes)
+        if gem_groups is not None:
+            return gem_groups
+
+    # Will be called only once.
+    def group_gems(self) -> list:
+        gem_groups = []
+        gem_indexes = self.agent.gem_indexes
+        while True:
+            gem_group = np.empty((0, 3))
+            searched_gems = np.empty((0, 3), dtype=int)
+            if len(gem_groups) == 0:
+                index = gem_indexes[0]
+            else:
+                flatten_groups = [x.tolist() for x in gem_groups]
+                index_ls = [x for x in gem_indexes.tolist() if x not in [m for y in flatten_groups for m in y]]
+                if len(index_ls) == 0:
+                    return gem_groups
+                index = index_ls[0]
+            gem_group = np.vstack((gem_group, index))
+            gem_groups = self.search_gems(index, gem_group, searched_gems, gem_groups, gem_indexes)
 
     def main(self):
-        self.calc_neighbor(self.agent.agent_index[0][0]  , self.agent.agent_index[0][1])
-        
+        # self.calc_neighbor(self.agent.agent_index[0][0], self.agent.agent_index[0][1])
         return Action.NOOP
         # return random.choice(
         #     [Action.DOWN, Action.DOWN_RIGHT, Action.DOWN_LEFT, Action.RIGHT, Action.LEFT, Action.UP_LEFT,
