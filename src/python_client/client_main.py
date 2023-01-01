506f2305baa3c21b84fc67107dffebf1c6c5c684
negarbarooti <barootinegar@gmail.com>
2023-01-01 10:35:09 +0330

    start phase 3

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -1,21 +1,14 @@
-import datetime
-
-from phase2 import Phase2
+import random
 from base import BaseAgent, Action
+
 
 
 class Agent(BaseAgent):
     # actions = [Action.RIGHT] + [Action.TELEPORT] * 100
     def do_turn(self) -> Action:
-        now1 = datetime.datetime.now()
-        phase2 = Phase2(self)
-        action = phase2.main()
-        now2 = datetime.datetime.now()
-        print(f'total_seconds: {(now2 - now1).total_seconds()}')
-        return action
-        # return random.choice(
-        #     [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.DOWN_RIGHT, Action.DOWN_LEFT, Action.UP_LEFT,
-        #      Action.UP_RIGHT, Action.NOOP])
+        return random.choice(
+            [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT, Action.DOWN_RIGHT, Action.DOWN_LEFT, Action.UP_LEFT,
+             Action.UP_RIGHT, Action.NOOP])
 
 
 if __name__ == '__main__':
--- src/python_client/utils/config.py
+++
@@ -1,6 +0,0 @@
-GEMS = {
-    "YELLOW_GEM": "1",
-    "GREEN_GEM": "2",
-    "RED_GEM": "3",
-    "BLUE_GEM": "4"
-}
