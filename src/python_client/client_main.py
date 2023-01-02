bfaed1b5b671427554bc263151c917711ec05a8c
mohsenamoon <dramatic225@gmail.com>
2023-01-02 23:59:38 +0330

    min max

--- src/python_client/client_main.py
+++ src/python_client/client_main.py
@@ -1,15 +1,14 @@
 import datetime
 
-from model_based_policy import ModelBasedPolicy
-from reinforce_learning import ReinforceLearning
 from base import BaseAgent, Action
+from MiniMax import MiniMax
 
 
 class Agent(BaseAgent):
     # actions = [Action.RIGHT] + [Action.TELEPORT] * 100
     def do_turn(self) -> Action:
         now1 = datetime.datetime.now()
-        phase2 = ModelBasedPolicy(self)
+        phase2 = MiniMax(self, (3, 5))
         # phase2 = ReinforceLearning(self)
         action = phase2.main()
         now2 = datetime.datetime.now()
